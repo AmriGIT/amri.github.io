3784fd2fb2e9d317d8883084635222ca70655a0f
rahmat.fathoni <rahmat.fathoni@danasaham.co.id>
2024-10-14 20:58:52 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="text-pretty text-gray-100 text-3xl">A.F</h1>
+      </div>
+      <div className="m- flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
